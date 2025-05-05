15ed8f2bc97134f36f98a40fc8eb778e8a687136
Andrew Williams <williams.ab@gmail.com>
2025-05-05 21:27:19 +0200

    Moved words file to root directory and expanded SeedWordsClass to open bip39-en.txt

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # main.py
 """
-This module serves as the entry point for the project, the main purpose of which is to learn how to code in VCS.
-It contains the main function that orcestrates the execution of the project i.e. - main; module1; module2
+This is the entry point for the project, the main purpose of which is to learn how to code in VCS.
+The main function that orcestrates the execution of the project i.e. - main; module1; module2 etc.
 """
 import logging
-# from modules.module1 import function_from_module1
-# from modules.module2 import class_from_module2
 from modules.seedwords import SeedWordClass
 
 # initialise logging - INFO, WARNING, ERROR, CRITICAL
@@ -17,23 +15,25 @@ logging.basicConfig(
 
 def main():
     """
-    This is the program entry point which calls all other functions.
+    main() program entry point.
     Returns : none
     """
-
     logging.info("Program started.")
 
     # result = function_from_module1()
     # logging.info(f"Result from module1: {result}")
-
     # instance = class_from_module2()
     # logging.info(f"Result from module2: {instance.message}")
 
     # ... rest of your code
     seed_words = SeedWordClass()
-    logging.info(f"Result from seedwords module: {seed_words.message}") 
+    # logging.info(f"Result from seedwords module: {seed_words.message}") 
 
-    seed_words.show_language()
+    seed_words.set_language("en")
+    # seed_words.show_language()
+    word_list = seed_words.get_word_list()
+
+    print(len(word_list))
 
     logging.info("Program terminated successfully.")
 
--- modules/seedwords.py
+++ modules/seedwords.py
@@ -4,7 +4,6 @@ import logging
 import os
 import binascii
 
-
 class SeedWordClass():
     """
     This class will handle access to the BIP39 seed words
@@ -14,15 +13,19 @@ class SeedWordClass():
     def __init__(self):
 
         self.message = "Inside a class in seedwords module"
-        self.language = "en"
+        self.language = "XX"
         logging.info("Instance of the SeedWords() class created.")
+
+    def set_language(self, language):
+        self.language = language
+        print(f"The Seed Word Language has been set to : - {self.language}")
 
 
     def show_language(self):
         print(f"The Seed Word Language is : - {self.language}")
     
 
-    def get_word_list(filename):
+    def get_word_list(self):
         """
         Returns the text contents of filename as a list.
         Assumes a text file input with one word per line.
@@ -30,7 +33,9 @@ class SeedWordClass():
         Returns : list
         """
         logging.info("Executing function get_word_list().")
-        
+        filename = "bip39-" + self.language + ".txt"
+        logging.info("The seed word language file has been set to : " + filename)
+
         word_list = []
         with open(filename, "rt", encoding="utf-8") as file:
             for line in file.readlines():
