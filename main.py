4ccdc38d94e246fd7add90d8fc25e427ed87f059
Andrew Williams <williams.ab@gmail.com>
2025-05-06 11:54:31 +0200

    Removed old unused code and added commented code for command-line arguments.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
 """
 This is the entry point for the project, the main purpose of which is to learn how to code in VCS.
-The main function that orcestrates the execution of the project i.e. - main; module1; module2 etc.
 """
+
 import logging
+# import argparse
+# import requests     # TESTING ONLY
+# import json         # TESTING ONLY
 from modules.seedwords import SeedWordClass
 
 # initialise logging - INFO, WARNING, ERROR, CRITICAL
@@ -20,16 +23,26 @@ def main():
     """
     logging.info("Program started.")
 
+    # # This example requires 'import argparse'.
+    # parser = argparse.ArgumentParser(description="Create a Bitcoin pass phrase.")
+    # parser.add_argument('filename', help='Word list file to be used.', type=str)
+    # parser.add_argument('second', help='second number', type=str)
+    # args = parser.parse_args()
+    # print(args)
+    # print(args.filename)        print(filename)
+
+    # print(args.second)
+
     # ... rest of your code
     seed_words = SeedWordClass()
-    
     seed_words.set_language("en")
-    seed_words.show_language()
+    print(f"The current seed word language is set to '{seed_words.get_language()}'." )
     word_list = seed_words.get_word_list()
 
     print(word_list[333])
 
     logging.info("Program terminated successfully.")
+    return    
 
 if __name__ == "__main__":
     main()
--- modules/seedwords.py
+++ modules/seedwords.py
@@ -7,34 +7,37 @@ import binascii
 class SeedWordClass():
     """
     This class will handle access to the BIP39 seed words
-    The single parameter that will be passed is the language.
+        set_language();     passed the two letter language code en, de, fr, es... etc.
+        get_language();     returns the two letter language code.
+        get_word_list();    returns a cleaned array of BIP39 words.
     Attributes : (str): message
     """
     def __init__(self):
-
-        self.message = "Inside a class in seedwords module"
+        self.message = "Inside SeedWordClass()"
         self.language = "XX"
-        logging.info("Instance of the SeedWords() class created.")
+        logging.info("Instance of SeedWordClass() created.")
 
     def set_language(self, language):
+        """Sets (and checks) the BIP39 language to en, fr, de, es, cn... etc"""
         self.language = language
-        print(f"The Seed Word Language has been set to : - {self.language}")
+        logging.info(f"The BIP39 seed word language has been set to : - {self.language}")
+        return
 
-
-    def show_language(self):
-        print(f"The Seed Word Language is : - {self.language}")
+    def get_language(self):
+        """ Returns the currently set BIP39 seed word language."""
+        # print(f"The Seed Word Language is : - {self.language}")
+        return self.language
     
-
     def get_word_list(self):
         """
-        Returns the text contents of filename as a list.
+        Returns the text contents of filename as an array.
         Assumes a text file input with one word per line.
-        Strips whitespace on output.
+        Strips whitespace and one word per element.
         Returns : list
         """
-        logging.info("Executing function get_word_list().")
+        # logging.info("Executing function get_word_list().")
         filename = "bip39-" + self.language + ".txt"
-        logging.info("The seed word language file has been set to : " + filename)
+        logging.info(f"The seed word language file has been set to : '{filename}' .")
 
         word_list = []
         with open(filename, "rt", encoding="utf-8") as file:
@@ -42,4 +45,4 @@ class SeedWordClass():
                 word_list.append(line.strip())
         return word_list
 
-''' Select language function required '''
+
